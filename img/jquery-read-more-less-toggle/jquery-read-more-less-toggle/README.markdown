9df2110ed149e7e662addf6db8696ca53a4888e7
rajashree <rajashree.alchemist@gmail.com>
2020-01-27 18:53:38 -0800

    project setup

---
+++ img/jquery-read-more-less-toggle/jquery-read-more-less-toggle/README.markdown
@@ -0,0 +1,4 @@
+# jQuery Read More/Less Toggle
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/maxds/pen/jgeoA](https://codepen.io/maxds/pen/jgeoA).
+
+ 
---
+++ img/jquery-read-more-less-toggle/jquery-read-more-less-toggle/dist/script.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 100;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "Show more >";
+    var lesstext = "Show less";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+
+
+
+
+            var rb = $(this).parent().parent();
+            // alert(asp);
+            // console.log(asp);
+            var rb_siblings = rb.siblings();
+
+            // console.log(rb_siblings);
+            rb_siblings.each(function() {
+
+                var content = $(this).html();
+
+                    var c = content.substr(0, showChar);
+                    var h = content.substr(showChar, content.length - showChar);
+         
+                    var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<p class="morelink">' + moretext + '</p></span>';
+         
+                    $(this).html(html);
+
+
+                $(this).find('.morelink').removeClass("less");
+                $(this).find('.morelink').html(moretext);
+                // var content = $(this).html();
+                // console.log(content);
+
+                
+            });
